6b2b2192e8139296852f020db0e0078445ef7ea6
Eben60 <8xe8n-cm@online.de>
2021-01-21 22:31:09 +0100

    moved files to this project

--- src/LabView0mqJlExamples.jl
+++ src/LabView0mqJlExamples.jl
@@ -1,5 +1,10 @@
 module LabView0mqJlExamples
 
 using LabView0mqJl
+using JSON3
+using Colors, TestImages, ImageIO, ImageShow, FileIO
+include("./user_functions.jl")
+
+export show_img, myf1, myf2, myf3, test_bin2nums,numarr_loopback, numarrs_lpbk, test_rgbimg
 
 end # module
